2328bf7e3162e90805b7707ed63fcedf455226df
DoMess <michael.lam14@outlook.com>
2025-02-27 20:02:15 +1100

    Calc settings, testParseInteger()

---
+++ calc/Calculator.swift
@@ -0,0 +1,18 @@
+//
+//  Calculator.swift
+//  calc
+//
+//  Created by Michael Lam on 27/2/2025.
+//  Copyright © 2025 UTS. All rights reserved.
+//
+
+class Calculator {
+    
+    func Addition(_ number1: Int, _ number2: Int) -> Int {
+        return number1 + number2
+    }
+    
+    func Subtraction(_ number1: Int, _ number2: Int) -> Int {
+        return number1 - number2
+    }
+}
--- calc/main.swift
+++ calc/main.swift
@@ -11,4 +11,22 @@ import Foundation
 var args = ProcessInfo.processInfo.arguments
 args.removeFirst() // remove the name of the program
 
-print(Int(args[0])!)
+var calculator : Calculator = Calculator.init()
+
+/// calculator rules
+/// - May be more than 2 numbers + 1 operator
+/// - Print integer
+
+// clear testParseInteger() - done
+if args.count == 1 {
+    print(Int(args[0])!)
+}
+
+switch args[1] {
+case "+":
+    print(calculator.Addition(Int(args[0])!, Int(args[2])!))
+default:
+    print(0)
+}
+
+//print(Int(args[0])!, (args[1]), Int(args[2])!, separator: " ")
